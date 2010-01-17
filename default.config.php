a5122d887f4ba58b414da3a8533776bfbfaa9322
Imre Mehesz <imehesz@gmail.com>
2010-01-17 01:35:29 -0500

    add jQuery (straight from Google) and configuration file - nothing exciting yet ...

---
+++ default.config.php
@@ -0,0 +1,16 @@
+<?php
+
+    /**
+     *
+     */
+	defined( 'LIMONADE_PATH' ) or define( 'LIMONADE_PATH','/path/to/your/limonade/lib' );
+
+    /**
+     *
+     */
+    defined( 'TWITTER_USER' ) or define( 'TWITTER_USER', 'your twitter username' );
+
+    /**
+     * 
+     */
+    defined( 'TWITTER_PASS' ) or define( 'TWITTER_PASS', 'your twitter password' );
--- index.php
+++ index.php
@@ -2,12 +2,27 @@
     require_once 'config.php';
     require_once LIMONADE_PATH .'limonade.php';
 
+    /**
+     *
+     */
     function configure()
     {
-      option('env', DEVELOPMENT);
+        $env = stristr( $_SERVER[ 'HTTP_HOST' ], 'local' ) ? ENV_DEVELOPMENT : ENV_PRODUCTION;
+        option('env', $env );
+
+        if( option('env') > ENV_PRODUCTION )
+        {
+            // some development settings ...
+        }
+        else
+        {
+            // some production settings ...
+        }
     }
 
-
+    /**
+     *
+     */
     function before()
     {
         layout('html_my_layout');
@@ -16,9 +31,12 @@
     dispatch('/', 'welcome');
         function welcome()
         {
-            return html('Welcome!');
+            return html( 'Welcome!' );
         }
 
+    /**
+     *
+     */
     function after($output)
     {
         $time = number_format( (float)substr(microtime(), 0, 10) - LIM_START_MICROTIME, 6);
@@ -32,10 +50,26 @@
     <html>
     <head>
         <title>Limonde first example</title>
+        <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.4/jquery.min.js"></script>
+        <script type="text/javascript" src="js/jquery-twitter-plugin.js"></script>
+        <script type="text/javascript">
+            $(document).ready(function() {
+                // This is more like it!
+                // $.twitter.search("roadrage",printSuccess);
+//                $.twitter.test(printSuccess);
+
+            });
+
+            function printSuccess(data, textStatus){
+                //Handle data as a JSON object
+//                alert( data );
+            }
+        </script>
     </head>
     <body>
     <h1>Roadfinger!</h1>
         <?=$content?>
+        <div id="printSuccess"></div>
     </body>
     </html>
     <?}?>
