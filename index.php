41b8166c8fb0f49f865a706e8b73e4890e0b12e4
Imre Mehesz <imehesz@gmail.com>
2010-03-05 23:39:31 -0500

    added about page and google tracking stuff

--- index.php
+++ index.php
@@ -59,6 +59,47 @@
             return html( $output );
         }
 
+    dispatch( '/about', 'about' );
+        function about()
+        {
+            return html(
+<<<HTML
+<div class="yellow box">
+    <strong>NO</strong> registration<br />
+    <strong>NO</strong> signup<br />
+    <strong>NO</strong> hidden fees<br />
+</div>
+
+<div class="aqua box">
+    use your <a alt="twitter.com" title="twitter.com" href="http://twitter.com">twitter</a> account,<br />
+    or get one!
+</div>
+
+<div class="lime box">
+    if someone ticks you off on the road, <br />
+    <strong>tweet</strong> it (use <strong>#roadfinger</strong> or <strong>#roadrage</strong> tags)
+</div>
+
+<div class="fuchsia box">
+    you can help us rank the <strong>donkeys</strong> by providing location information using this example:
+</div>
+
+<div class="aqua box" style="width:300px;">
+<strong>#roadfinger</strong> to the SOB in the yellow mustang
+t:GH67Y3 <span class="mini-info">(tag)</span> in
+c:Smallville <span class="mini-info">(city)</span>
+z:12345 <span class="mini-info">(zip code)</span>
+ for smashing my car this afternoon
+</div>
+
+
+<div class="yellow box">
+thanks, and have fun ;)
+</div>
+HTML
+            );
+        }
+
     dispatch( '/list', 'listTweets' );
         function listTweets()
         {
@@ -110,8 +151,8 @@
             <div class="wrapper-inside">
                 <div class="menu">
                     <div class="menu-item"><a href="/"><img src="images/url.png" alt="RoadFinger.mehesz.net" title="RoadFinger.mehesz.net" border="0"/></a></div>
-                    <div class="menu-item"><img src="images/about.png" /></div>
-                    <div class="menu-item"><img src="images/contact.png" /></div>
+                    <div class="menu-item"><a href="<?php echo url_for( 'about' );?>" alt="about" title="about"><img src="images/about.png" /></a></div>
+                    <div class="menu-item"><a alt="contact" title="contact" href="javascript:void();" onclick="alert('info [at] mehesz.net');"><img src="images/contact.png" /></a></div>
                     <div class="menu-item"><a href="http://limonade.sofa-design.net/"><img alt="Limonade PHP Framework" title="Limonade PHP Framework" src="images/limonade.png" border="0"/></a></div>
                     <div style="clear:both;"></div>
                 </div>
@@ -120,7 +161,7 @@
                         <p>
                             <br /><br /><br /><br /><br />
                         </p>
-                        <img src="images/tank.png" style="vertical-align:bottom;" />
+                        <img alt="this is just a tank" title="this is just a tank" src="images/tank.png" style="vertical-align:bottom;" />
                     </div>
                     <div class="content-right"><?php print $content?></div>
                     <div style="clear:both;"></div>
@@ -129,8 +170,19 @@
                     <img src="images/bg-bottom.png" />
                 </div>
             </div>
-            <div style="text-align:center;font-size:10px;color:#999;">mehesz.net</div>
+            <div class="footer">
+                &copy; <a href="http://mehesz.net" alt="mehesz.net - open source for life" title="mehesz.net - open source for life">mehesz<span style="color:#f00;">.</span>net</a> <?php print date('Y', time()); ?>
+            </div>
         </div>
+        <script type="text/javascript">
+        var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+        document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+        </script>
+        <script type="text/javascript">
+        try {
+        var pageTracker = _gat._getTracker("UA-5417349-4");
+        pageTracker._trackPageview();
+        } catch(err) {}</script>
     </body>
     </html>
     <?}?>
